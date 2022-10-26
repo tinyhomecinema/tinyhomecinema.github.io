5b64122c646f93e307ce6f98489e9d7c5ebf603a
Cineminha <tinyhomecinema@gmail.com>
2022-10-26 15:43:19 -0300

    Added all watched films to film data, selecting on the last 10 in NOW SHOWING section

--- data/films.js
+++ data/films.js
@@ -8,5 +8,13 @@ var films = [
   ['https://letterboxd.com/hpfilho/film/devs/', 'https://a.ltrbxd.com/resized/film-poster/6/0/5/7/0/1/605701-devs-0-600-0-900-crop.jpg?v=85e89ce4e8'],
   ['https://letterboxd.com/hpfilho/film/the-worlds-end/', 'https://a.ltrbxd.com/resized/film-poster/8/6/9/9/1/86991-the-world-s-end-0-600-0-900-crop.jpg?v=48a7252f0d'],
   ['https://letterboxd.com/hpfilho/film/everything-everywhere-all-at-once/', 'https://a.ltrbxd.com/resized/film-poster/4/7/4/4/7/4/474474-everything-everywhere-all-at-once-0-600-0-900-crop.jpg?v=281f1a041e'],
-  ['https://letterboxd.com/hpfilho/film/top-gun/', 'https://a.ltrbxd.com/resized/sm/upload/e9/dh/nn/5w/fKSZl8ONN7DQh5ujgdObRIFmhiZ-0-600-0-900-crop.jpg?v=042d149015']
+  ['https://letterboxd.com/hpfilho/film/top-gun/', 'https://a.ltrbxd.com/resized/sm/upload/e9/dh/nn/5w/fKSZl8ONN7DQh5ujgdObRIFmhiZ-0-600-0-900-crop.jpg?v=042d149015'],
+  ['https://letterboxd.com/hpfilho/film/prey-2022/', 'https://a.ltrbxd.com/resized/film-poster/6/8/6/3/8/9/686389-prey-0-600-0-900-crop.jpg?v=f9f0c6bb6e'],
+  ['https://letterboxd.com/hpfilho/film/take-shelter/', 'https://a.ltrbxd.com/resized/sm/upload/h8/rx/g1/tm/jFHe1RSju7m4EyRr7fP7QGq8QpO-0-600-0-900-crop.jpg?v=15890185ed'],
+  ['https://letterboxd.com/hpfilho/film/lamb-2021/', 'https://a.ltrbxd.com/resized/film-poster/7/0/7/1/1/4/707114-lamb-0-600-0-900-crop.jpg?v=06329d82b2'],
+  ['https://letterboxd.com/hpfilho/film/what-ever-happened-to-baby-jane/', 'https://a.ltrbxd.com/resized/sm/upload/cq/ht/cp/b9/7Zr0EuDMcIvnJREsLmaAWi5x7mV-0-600-0-900-crop.jpg?v=2a2340c9fd'],
+  ['https://letterboxd.com/hpfilho/film/palm-springs-2020/', 'https://a.ltrbxd.com/resized/film-poster/5/1/5/4/6/7/515467-palm-springs-0-600-0-900-crop.jpg?v=d3df94888b'],
+  ['https://letterboxd.com/hpfilho/film/eternals/', 'https://a.ltrbxd.com/resized/film-poster/4/5/4/0/1/6/454016-eternals-0-600-0-900-crop.jpg?v=2578d04fd8'],
+  ['https://letterboxd.com/hpfilho/film/west-side-story-2021/', 'https://a.ltrbxd.com/resized/sm/upload/yd/j5/tz/up/xW3t04p24jH7aQyq1J63ApAP9gG-0-600-0-900-crop.jpg?v=a25d5f6f88'],
+  ['https://letterboxd.com/hpfilho/film/ghostbusters-afterlife/', 'https://a.ltrbxd.com/resized/film-poster/3/5/9/3/4/9/359349-ghostbusters-afterlife-0-600-0-900-crop.jpg?v=b306cafe34']
 ]
