5866769e29adbea5f1d442120044533273d75d59
Cineminha <tinyhomecinema@gmail.com>
2023-04-10 15:14:04 -0300

    Created data files for film collections and streaming services

---
+++ data/collection.js
@@ -0,0 +1,9 @@
+var collection = [
+  ['Films', 317, 'https://letterboxd.com/hpfilho/list/my-film-collection/detail/', 'collection'],
+  ['Miniseries', 2, 'https://letterboxd.com/hpfilho/list/miniseries/detail/', 'collection'],
+  ['Blu-ray', 109, 'https://letterboxd.com/hpfilho/list/blu-ray/', 'media'],
+  ['DVD', 261, 'https://letterboxd.com/hpfilho/list/dvd/', 'media'],
+  ['BD/DVD', 53, 'https://letterboxd.com/hpfilho/list/blu-ray-dvd/', 'media'],
+  ['Dolby Atmos', 14, 'https://letterboxd.com/hpfilho/list/dolby-atmos/', 'audio'],
+  ['DTS X', 5, 'https://letterboxd.com/hpfilho/list/dts-x/', 'audio']
+]
