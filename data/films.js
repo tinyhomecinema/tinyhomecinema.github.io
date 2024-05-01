8465255428e978f94effcbb64dac98e9c83f6022
Cineminha <tinyhomecinema@gmail.com>
2024-05-01 19:00:19 -0300

    [auto] Updated data

--- data/films.js
+++ data/films.js
@@ -1,4 +1,5 @@
 var films = [
+  ['May December', '2023', 'https://letterboxd.com/tinyhomecinema/film/may-december/', 'https://a.ltrbxd.com/resized/sm/upload/zh/p4/cg/6v/may-december-0-600-0-900-crop.jpg'],
   ['Perfect Days', '2023', 'https://letterboxd.com/tinyhomecinema/film/perfect-days-2023/', 'https://a.ltrbxd.com/resized/film-poster/8/7/9/2/2/9/879229-perfect-days-0-600-0-900-crop.jpg'],
   ['The Zone of Interest', '2023', 'https://letterboxd.com/tinyhomecinema/film/the-zone-of-interest/', 'https://a.ltrbxd.com/resized/film-poster/3/9/8/8/0/0/398800-the-zone-of-interest-0-600-0-900-crop.jpg'],
   ['Anatomy of a Fall', '2023', 'https://letterboxd.com/tinyhomecinema/film/anatomy-of-a-fall/', 'https://a.ltrbxd.com/resized/film-poster/8/2/2/0/9/3/822093-anatomy-of-a-fall-0-600-0-900-crop.jpg'],
@@ -8,5 +9,4 @@ var films = [
   ['Empire of Light', '2022', 'https://letterboxd.com/tinyhomecinema/film/empire-of-light/', 'https://a.ltrbxd.com/resized/film-poster/7/3/1/2/8/6/731286-empire-of-light-0-600-0-900-crop.jpg'],
   ['The Hijacking of Flight 375', '2023', 'https://letterboxd.com/tinyhomecinema/film/the-hijacking-of-flight-375/', 'https://a.ltrbxd.com/resized/film-poster/9/3/1/1/4/2/931142-the-hijacking-of-flight-375-0-600-0-900-crop.jpg'],
   ['Society of the Snow', '2023', 'https://letterboxd.com/tinyhomecinema/film/society-of-the-snow/', 'https://a.ltrbxd.com/resized/film-poster/8/1/3/8/4/0/813840-society-of-the-snow-0-600-0-900-crop.jpg'],
-  ['The Abandoned', '2022', 'https://letterboxd.com/tinyhomecinema/film/the-abandoned-2022/', 'https://a.ltrbxd.com/resized/film-poster/9/0/7/0/2/6/907026-the-abandoned-0-600-0-900-crop.jpg'],
 ]
