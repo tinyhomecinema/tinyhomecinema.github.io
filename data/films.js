ec5d3f05d706c8e907ab3ca54e6d70175f28bb57
Cineminha <tinyhomecinema@gmail.com>
2024-06-23 19:00:24 -0300

    [auto] Updated data

--- data/films.js
+++ data/films.js
@@ -1,4 +1,5 @@
 var films = [
+  ['Past Lives', '2023', 'https://letterboxd.com/tinyhomecinema/film/past-lives/', 'https://a.ltrbxd.com/resized/film-poster/5/9/1/0/5/3/591053-past-lives-0-600-0-900-crop.jpg'],
   ['When Evil Lurks', '2023', 'https://letterboxd.com/tinyhomecinema/film/when-evil-lurks/', 'https://a.ltrbxd.com/resized/film-poster/6/6/6/3/9/6/666396-when-evil-lurks-0-600-0-900-crop.jpg'],
   ['Cocaine Bear', '2023', 'https://letterboxd.com/tinyhomecinema/film/cocaine-bear/', 'https://a.ltrbxd.com/resized/film-poster/7/2/1/3/3/3/721333-cocaine-bear-0-600-0-900-crop.jpg'],
   ['May December', '2023', 'https://letterboxd.com/tinyhomecinema/film/may-december/', 'https://a.ltrbxd.com/resized/sm/upload/zh/p4/cg/6v/may-december-0-600-0-900-crop.jpg'],
@@ -8,5 +9,4 @@ var films = [
   ['American Fiction', '2023', 'https://letterboxd.com/tinyhomecinema/film/american-fiction/', 'https://a.ltrbxd.com/resized/film-poster/9/5/2/8/1/2/952812-american-fiction-0-600-0-900-crop.jpg'],
   ['Flora and Son', '2023', 'https://letterboxd.com/tinyhomecinema/film/flora-and-son/', 'https://a.ltrbxd.com/resized/film-poster/9/5/5/6/4/0/955640-flora-and-son-0-600-0-900-crop.jpg'],
   ['Fire of Love', '2022', 'https://letterboxd.com/tinyhomecinema/film/fire-of-love-2022/', 'https://a.ltrbxd.com/resized/film-poster/8/1/9/8/6/3/819863-fire-of-love-0-600-0-900-crop.jpg'],
-  ['Empire of Light', '2022', 'https://letterboxd.com/tinyhomecinema/film/empire-of-light/', 'https://a.ltrbxd.com/resized/film-poster/7/3/1/2/8/6/731286-empire-of-light-0-600-0-900-crop.jpg'],
 ]
