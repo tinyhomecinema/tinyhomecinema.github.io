ead199d331ecda6bc5fcf0610849c517005a5ae3
Cineminha <tinyhomecinema@gmail.com>
2024-08-03 21:00:24 -0300

    [auto] Updated data

--- data/films.js
+++ data/films.js
@@ -1,4 +1,5 @@
 var films = [
+  ['Under Paris', '2024', 'https://letterboxd.com/tinyhomecinema/film/under-paris/', 'https://a.ltrbxd.com/resized/film-poster/9/0/2/0/5/8/902058-under-paris-0-600-0-900-crop.jpg'],
   ['The Holdovers', '2023', 'https://letterboxd.com/tinyhomecinema/film/the-holdovers/', 'https://a.ltrbxd.com/resized/film-poster/7/5/5/5/6/4/755564-the-holdovers-0-600-0-900-crop.jpg'],
   ['Past Lives', '2023', 'https://letterboxd.com/tinyhomecinema/film/past-lives/', 'https://a.ltrbxd.com/resized/film-poster/5/9/1/0/5/3/591053-past-lives-0-600-0-900-crop.jpg'],
   ['When Evil Lurks', '2023', 'https://letterboxd.com/tinyhomecinema/film/when-evil-lurks/', 'https://a.ltrbxd.com/resized/film-poster/6/6/6/3/9/6/666396-when-evil-lurks-0-600-0-900-crop.jpg'],
@@ -8,5 +9,4 @@ var films = [
   ['The Zone of Interest', '2023', 'https://letterboxd.com/tinyhomecinema/film/the-zone-of-interest/', 'https://a.ltrbxd.com/resized/film-poster/3/9/8/8/0/0/398800-the-zone-of-interest-0-600-0-900-crop.jpg'],
   ['Anatomy of a Fall', '2023', 'https://letterboxd.com/tinyhomecinema/film/anatomy-of-a-fall/', 'https://a.ltrbxd.com/resized/film-poster/8/2/2/0/9/3/822093-anatomy-of-a-fall-0-600-0-900-crop.jpg'],
   ['American Fiction', '2023', 'https://letterboxd.com/tinyhomecinema/film/american-fiction/', 'https://a.ltrbxd.com/resized/film-poster/9/5/2/8/1/2/952812-american-fiction-0-600-0-900-crop.jpg'],
-  ['Flora and Son', '2023', 'https://letterboxd.com/tinyhomecinema/film/flora-and-son/', 'https://a.ltrbxd.com/resized/film-poster/9/5/5/6/4/0/955640-flora-and-son-0-600-0-900-crop.jpg'],
 ]
