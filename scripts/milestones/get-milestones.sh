e8ab47c8c4e56d78cee9e62af7bb51034c47fd8f
Cineminha <tinyhomecinema@gmail.com>
2024-08-25 16:07:55 -0300

    Updated scripts

--- scripts/milestones/get-milestones.sh
+++ scripts/milestones/get-milestones.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-cd /home/pi/github/cineminha_web/
+cd /home/pi/github/tinyhomecinema.github.io/
 
 git pull origin main
 
-cd /home/pi/github/cineminha_web/scripts/milestones/html
+cd /home/pi/github/tinyhomecinema.github.io/scripts/milestones/html
 
 wget 'https://letterboxd.com/tinyhomecinema/list/milestones/'
 mv index.html milestones.html
@@ -30,6 +30,6 @@ mv index.html first-showing.html
 wget 'https://letterboxd.com/tinyhomecinema/tag/rewatch/diary/'
 mv index.html rewatch.html
 
-cd /home/pi/github/cineminha_web/scripts/milestones
+cd /home/pi/github/tinyhomecinema.github.io/scripts/milestones
 
 ./get-milestones.pl
