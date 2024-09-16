8f85d4ca1b0fd02dc8206bca2af7c2302d97f819
Cineminha <tinyhomecinema@gmail.com>
2024-09-16 19:39:09 -0300

    Added link to "HDR10 Pro" on data/techs.js

--- data/techs.js
+++ data/techs.js
@@ -5,5 +5,5 @@ var audio = [
 
 var video = [
   ['Dolby Vision', 'https://www.dolby.com/technologies/dolby-vision'],
-  ['HDR10 Pro', ''],
+  ['HDR10 Pro', 'https://www.lg.com/in/lgoled2019/4k_hdr/'],
 ]
