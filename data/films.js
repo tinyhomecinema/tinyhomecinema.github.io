6a1c10c56560790fad453c8281aa9432761f0d5f
Cineminha <tinyhomecinema@gmail.com>
2024-10-05 21:00:29 -0300

    [auto] Updated data

--- data/films.js
+++ data/films.js
@@ -1,4 +1,5 @@
 var films = [
+  ['Late Night with the Devil', '2023', 'https://letterboxd.com/tinyhomecinema/film/late-night-with-the-devil/', 'https://a.ltrbxd.com/resized/film-poster/8/4/3/4/1/5/843415-late-night-with-the-devil-0-600-0-900-crop.jpg'],
   ['Immaculate', '2024', 'https://letterboxd.com/tinyhomecinema/film/immaculate-2024/', 'https://a.ltrbxd.com/resized/film-poster/9/3/8/8/9/4/938894-immaculate-0-600-0-900-crop.jpg'],
   ['The Watchers', '2024', 'https://letterboxd.com/tinyhomecinema/film/the-watchers-2024/', 'https://a.ltrbxd.com/resized/film-poster/9/7/9/7/8/3/979783-the-watchers-0-600-0-900-crop.jpg'],
   ['Under Paris', '2024', 'https://letterboxd.com/tinyhomecinema/film/under-paris/', 'https://a.ltrbxd.com/resized/film-poster/9/0/2/0/5/8/902058-under-paris-0-600-0-900-crop.jpg'],
@@ -8,5 +9,4 @@ var films = [
   ['Cocaine Bear', '2023', 'https://letterboxd.com/tinyhomecinema/film/cocaine-bear/', 'https://a.ltrbxd.com/resized/film-poster/7/2/1/3/3/3/721333-cocaine-bear-0-600-0-900-crop.jpg'],
   ['May December', '2023', 'https://letterboxd.com/tinyhomecinema/film/may-december/', 'https://a.ltrbxd.com/resized/sm/upload/zh/p4/cg/6v/may-december-0-600-0-900-crop.jpg'],
   ['Perfect Days', '2023', 'https://letterboxd.com/tinyhomecinema/film/perfect-days-2023/', 'https://a.ltrbxd.com/resized/film-poster/8/7/9/2/2/9/879229-perfect-days-0-600-0-900-crop.jpg'],
-  ['The Zone of Interest', '2023', 'https://letterboxd.com/tinyhomecinema/film/the-zone-of-interest/', 'https://a.ltrbxd.com/resized/film-poster/3/9/8/8/0/0/398800-the-zone-of-interest-0-600-0-900-crop.jpg'],
 ]
