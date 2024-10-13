a47e4114688124f916702605f9381296bfb09ede
Cineminha <tinyhomecinema@gmail.com>
2024-10-13 17:04:07 -0300

    [auto] Updated data

--- data/films.js
+++ data/films.js
@@ -1,4 +1,5 @@
 var films = [
+  ['Caddo Lake', '2024', 'https://letterboxd.com/tinyhomecinema/film/caddo-lake/', 'https://a.ltrbxd.com/resized/film-poster/7/7/7/9/7/9/777979-caddo-lake-0-600-0-900-crop.jpg'],
   ['Late Night with the Devil', '2023', 'https://letterboxd.com/tinyhomecinema/film/late-night-with-the-devil/', 'https://a.ltrbxd.com/resized/film-poster/8/4/3/4/1/5/843415-late-night-with-the-devil-0-600-0-900-crop.jpg'],
   ['Immaculate', '2024', 'https://letterboxd.com/tinyhomecinema/film/immaculate-2024/', 'https://a.ltrbxd.com/resized/film-poster/9/3/8/8/9/4/938894-immaculate-0-600-0-900-crop.jpg'],
   ['The Watchers', '2024', 'https://letterboxd.com/tinyhomecinema/film/the-watchers-2024/', 'https://a.ltrbxd.com/resized/film-poster/9/7/9/7/8/3/979783-the-watchers-0-600-0-900-crop.jpg'],
@@ -8,5 +9,4 @@ var films = [
   ['When Evil Lurks', '2023', 'https://letterboxd.com/tinyhomecinema/film/when-evil-lurks/', 'https://a.ltrbxd.com/resized/film-poster/6/6/6/3/9/6/666396-when-evil-lurks-0-600-0-900-crop.jpg'],
   ['Cocaine Bear', '2023', 'https://letterboxd.com/tinyhomecinema/film/cocaine-bear/', 'https://a.ltrbxd.com/resized/film-poster/7/2/1/3/3/3/721333-cocaine-bear-0-600-0-900-crop.jpg'],
   ['May December', '2023', 'https://letterboxd.com/tinyhomecinema/film/may-december/', 'https://a.ltrbxd.com/resized/sm/upload/zh/p4/cg/6v/may-december-0-600-0-900-crop.jpg'],
-  ['Perfect Days', '2023', 'https://letterboxd.com/tinyhomecinema/film/perfect-days-2023/', 'https://a.ltrbxd.com/resized/film-poster/8/7/9/2/2/9/879229-perfect-days-0-600-0-900-crop.jpg'],
 ]
