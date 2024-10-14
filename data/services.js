4823bdf9256e5a0ea13c4745a727df73327dbdab
Cineminha <tinyhomecinema@gmail.com>
2024-10-14 09:43:45 -0300

    Removed MGM+ and Globoplay from Current Services

--- data/services.js
+++ data/services.js
@@ -3,7 +3,7 @@ var services = [
   ['Prime Video', 'https://letterboxd.com/tinyhomecinema/tag/prime-video/films/', 'current'],
   ['Max', 'https://letterboxd.com/tinyhomecinema/tag/max/films/', 'current'],
   ['Apple TV+', 'https://letterboxd.com/tinyhomecinema/tag/apple-tv-/films/', 'current'],
-  ['Globoplay', 'https://letterboxd.com/tinyhomecinema/tag/globoplay/films/', 'current'],
+  ['Globoplay', 'https://letterboxd.com/tinyhomecinema/tag/globoplay/films/', ''],
   ['STAR+', 'https://letterboxd.com/tinyhomecinema/tag/star-/films/', 'rebranded'],
   ['Disney+', 'https://letterboxd.com/tinyhomecinema/tag/disney-/films/', 'past'],
   ['Filmicca', 'https://letterboxd.com/tinyhomecinema/tag/filmicca/films/', 'current'],
@@ -13,7 +13,7 @@ var services = [
   ['Looke', 'https://letterboxd.com/tinyhomecinema/tag/looke/films/', ''],
   ['Oldflix', 'https://letterboxd.com/tinyhomecinema/tag/oldflix/films/', ''],
   ['Telecine', 'https://letterboxd.com/tinyhomecinema/tag/telecine/films/', 'past'],
-  ['MGM+', 'https://letterboxd.com/tinyhomecinema/tag/mgm-/films/', 'current'],
+  ['MGM+', 'https://letterboxd.com/tinyhomecinema/tag/mgm-/films/', ''],
   ['HBO Max', 'https://letterboxd.com/tinyhomecinema/tag/hbo-max/films/', 'rebranded'],
   ['Lionsgate+', 'https://letterboxd.com/tinyhomecinema/tag/lionsgate-/films/', 'closed'],
 ]
