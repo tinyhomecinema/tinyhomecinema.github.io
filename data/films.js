051b02e93e1c3a9c82aa52eb819d818265a31816
Cineminha <tinyhomecinema@gmail.com>
2024-11-24 19:00:22 -0300

    [auto] Updated data

--- data/films.js
+++ data/films.js
@@ -1,4 +1,5 @@
 var films = [
+  ['Wicked Little Letters', '2023', 'https://letterboxd.com/tinyhomecinema/film/wicked-little-letters/', 'https://a.ltrbxd.com/resized/film-poster/8/7/8/3/4/1/878341-wicked-little-letters-0-600-0-900-crop.jpg'],
   ['A Mother\'s Embrace', '2024', 'https://letterboxd.com/tinyhomecinema/film/a-mothers-embrace/', 'https://a.ltrbxd.com/resized/film-poster/9/5/0/9/3/3/950933-a-mothers-embrace-0-600-0-900-crop.jpg'],
   ['Ghostbusters: Frozen Empire', '2024', 'https://letterboxd.com/tinyhomecinema/film/ghostbusters-frozen-empire/', 'https://a.ltrbxd.com/resized/film-poster/8/7/1/0/4/1/871041-ghostbusters-frozen-empire-0-600-0-900-crop.jpg'],
   ['Monkey Man', '2024', 'https://letterboxd.com/tinyhomecinema/film/monkey-man/', 'https://a.ltrbxd.com/resized/film-poster/4/8/8/7/5/1/488751-monkey-man-0-600-0-900-crop.jpg'],
@@ -8,5 +9,4 @@ var films = [
   ['The Watchers', '2024', 'https://letterboxd.com/tinyhomecinema/film/the-watchers-2024/', 'https://a.ltrbxd.com/resized/film-poster/9/7/9/7/8/3/979783-the-watchers-0-600-0-900-crop.jpg'],
   ['Under Paris', '2024', 'https://letterboxd.com/tinyhomecinema/film/under-paris/', 'https://a.ltrbxd.com/resized/film-poster/9/0/2/0/5/8/902058-under-paris-0-600-0-900-crop.jpg'],
   ['The Holdovers', '2023', 'https://letterboxd.com/tinyhomecinema/film/the-holdovers/', 'https://a.ltrbxd.com/resized/film-poster/7/5/5/5/6/4/755564-the-holdovers-0-600-0-900-crop.jpg'],
-  ['Past Lives', '2023', 'https://letterboxd.com/tinyhomecinema/film/past-lives/', 'https://a.ltrbxd.com/resized/film-poster/5/9/1/0/5/3/591053-past-lives-0-600-0-900-crop.jpg'],
 ]
