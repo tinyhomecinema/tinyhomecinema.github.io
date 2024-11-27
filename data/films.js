4e71763f0461c6c62fe823fd1ee82029ffeb98d6
Cineminha <tinyhomecinema@gmail.com>
2024-11-26 21:01:27 -0300

    [auto] Updated data

--- data/films.js
+++ data/films.js
@@ -1,5 +1,5 @@
 var films = [
-  ['Wicked Little Letters', '2023', 'https://letterboxd.com/tinyhomecinema/film/wicked-little-letters/', 'https://a.ltrbxd.com/resized/film-poster/8/7/8/3/4/1/878341-wicked-little-letters-0-600-0-900-crop.jpg'],
+  ['Wicked Little Letters', '2023', 'https://letterboxd.com/tinyhomecinema/film/wicked-little-letters/1/', 'https://a.ltrbxd.com/resized/film-poster/8/7/8/3/4/1/878341-wicked-little-letters-0-600-0-900-crop.jpg'],
   ['A Mother\'s Embrace', '2024', 'https://letterboxd.com/tinyhomecinema/film/a-mothers-embrace/', 'https://a.ltrbxd.com/resized/film-poster/9/5/0/9/3/3/950933-a-mothers-embrace-0-600-0-900-crop.jpg'],
   ['Ghostbusters: Frozen Empire', '2024', 'https://letterboxd.com/tinyhomecinema/film/ghostbusters-frozen-empire/', 'https://a.ltrbxd.com/resized/film-poster/8/7/1/0/4/1/871041-ghostbusters-frozen-empire-0-600-0-900-crop.jpg'],
   ['Monkey Man', '2024', 'https://letterboxd.com/tinyhomecinema/film/monkey-man/', 'https://a.ltrbxd.com/resized/film-poster/4/8/8/7/5/1/488751-monkey-man-0-600-0-900-crop.jpg'],
@@ -9,4 +9,12 @@ var films = [
   ['The Watchers', '2024', 'https://letterboxd.com/tinyhomecinema/film/the-watchers-2024/', 'https://a.ltrbxd.com/resized/film-poster/9/7/9/7/8/3/979783-the-watchers-0-600-0-900-crop.jpg'],
   ['Under Paris', '2024', 'https://letterboxd.com/tinyhomecinema/film/under-paris/', 'https://a.ltrbxd.com/resized/film-poster/9/0/2/0/5/8/902058-under-paris-0-600-0-900-crop.jpg'],
   ['The Holdovers', '2023', 'https://letterboxd.com/tinyhomecinema/film/the-holdovers/', 'https://a.ltrbxd.com/resized/film-poster/7/5/5/5/6/4/755564-the-holdovers-0-600-0-900-crop.jpg'],
+  ['Past Lives', '2023', 'https://letterboxd.com/tinyhomecinema/film/past-lives/', 'https://a.ltrbxd.com/resized/film-poster/5/9/1/0/5/3/591053-past-lives-0-600-0-900-crop.jpg'],
+  ['When Evil Lurks', '2023', 'https://letterboxd.com/tinyhomecinema/film/when-evil-lurks/', 'https://a.ltrbxd.com/resized/film-poster/6/6/6/3/9/6/666396-when-evil-lurks-0-600-0-900-crop.jpg'],
+  ['Cocaine Bear', '2023', 'https://letterboxd.com/tinyhomecinema/film/cocaine-bear/', 'https://a.ltrbxd.com/resized/film-poster/7/2/1/3/3/3/721333-cocaine-bear-0-600-0-900-crop.jpg'],
+  ['May December', '2023', 'https://letterboxd.com/tinyhomecinema/film/may-december/', 'https://a.ltrbxd.com/resized/sm/upload/zh/p4/cg/6v/may-december-0-600-0-900-crop.jpg'],
+  ['Perfect Days', '2023', 'https://letterboxd.com/tinyhomecinema/film/perfect-days-2023/', 'https://a.ltrbxd.com/resized/film-poster/8/7/9/2/2/9/879229-perfect-days-0-600-0-900-crop.jpg'],
+  ['The Zone of Interest', '2023', 'https://letterboxd.com/tinyhomecinema/film/the-zone-of-interest/', 'https://a.ltrbxd.com/resized/film-poster/3/9/8/8/0/0/398800-the-zone-of-interest-0-600-0-900-crop.jpg'],
+  ['Anatomy of a Fall', '2023', 'https://letterboxd.com/tinyhomecinema/film/anatomy-of-a-fall/', 'https://a.ltrbxd.com/resized/film-poster/8/2/2/0/9/3/822093-anatomy-of-a-fall-0-600-0-900-crop.jpg'],
+  ['American Fiction', '2023', 'https://letterboxd.com/tinyhomecinema/film/american-fiction/', 'https://a.ltrbxd.com/resized/film-poster/9/5/2/8/1/2/952812-american-fiction-0-600-0-900-crop.jpg'],
 ]
