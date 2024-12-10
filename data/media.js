d1623738f3e410892c51b222ebda6a0ae2f2627f
Cineminha <tinyhomecinema@gmail.com>
2024-12-10 17:00:18 -0300

    [auto] Updated data

--- data/media.js
+++ data/media.js
@@ -1,6 +1,6 @@
 var shelves = [
   ['Movies', 414, 'https://letterboxd.com/tinyhomecinema/list/movies-shelf/', 'shelf'],
-  ['TV Shows', 17, 'https://trakt.tv/users/cineminha_shelf/lists/tv-show-collection', 'shelf'],
+  ['TV Shows', 18, 'https://trakt.tv/users/cineminha_shelf/lists/tv-show-collection', 'shelf'],
   ['Music', 35, 'https://www.librarything.com/catalog.php?view=cineminha&collection=898678&shelf=shelf&sort=stampREV', 'shelf'],
   ['Books', 27, 'https://www.librarything.com/catalog.php?view=cineminha&collection=898806&shelf=shelf', 'shelf']
 ]
@@ -18,7 +18,7 @@ var collection = [
 ]
 
 var tv_collection = [
-  ['Blu-ray', 15, 'https://trakt.tv/users/cineminha_shelf/lists/tv-show-blu-rays', 'media'],
+  ['Blu-ray', 16, 'https://trakt.tv/users/cineminha_shelf/lists/tv-show-blu-rays', 'media'],
   ['DVD', 3, 'https://trakt.tv/users/cineminha_shelf/lists/tv-show-dvds', 'media'],
-  ['Dolby Atmos', 5, 'https://trakt.tv/users/cineminha_shelf/lists/dolby-atmos', 'audio'],
+  ['Dolby Atmos', 6, 'https://trakt.tv/users/cineminha_shelf/lists/dolby-atmos', 'audio'],
 ]
