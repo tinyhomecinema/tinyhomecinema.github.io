09c43af5dd9cb1683f3b01659ffd5e64666c0c2e
Cineminha <tinyhomecinema@gmail.com>
2024-12-14 19:00:26 -0300

    [auto] Updated data

--- data/films.js
+++ data/films.js
@@ -1,4 +1,5 @@
 var films = [
+  ['Fly Me to the Moon', '2024', 'https://letterboxd.com/tinyhomecinema/film/fly-me-to-the-moon-2024/', 'https://a.ltrbxd.com/resized/film-poster/8/6/1/0/3/9/861039-fly-me-to-the-moon-0-600-0-900-crop.jpg'],
   ['Asteroid City', '2023', 'https://letterboxd.com/tinyhomecinema/film/asteroid-city/', 'https://a.ltrbxd.com/resized/film-poster/6/6/8/5/5/5/668555-asteroid-city-0-600-0-900-crop.jpg'],
   ['Trap', '2024', 'https://letterboxd.com/tinyhomecinema/film/trap-2024/', 'https://a.ltrbxd.com/resized/film-poster/9/3/1/1/6/4/931164-trap-0-600-0-900-crop.jpg'],
   ['Wicked Little Letters', '2023', 'https://letterboxd.com/tinyhomecinema/film/wicked-little-letters/1/', 'https://a.ltrbxd.com/resized/film-poster/8/7/8/3/4/1/878341-wicked-little-letters-0-600-0-900-crop.jpg'],
@@ -8,5 +9,4 @@ var films = [
   ['Caddo Lake', '2024', 'https://letterboxd.com/tinyhomecinema/film/caddo-lake/', 'https://a.ltrbxd.com/resized/film-poster/7/7/7/9/7/9/777979-caddo-lake-0-600-0-900-crop.jpg'],
   ['Late Night with the Devil', '2023', 'https://letterboxd.com/tinyhomecinema/film/late-night-with-the-devil/', 'https://a.ltrbxd.com/resized/film-poster/8/4/3/4/1/5/843415-late-night-with-the-devil-0-600-0-900-crop.jpg'],
   ['Immaculate', '2024', 'https://letterboxd.com/tinyhomecinema/film/immaculate-2024/', 'https://a.ltrbxd.com/resized/film-poster/9/3/8/8/9/4/938894-immaculate-0-600-0-900-crop.jpg'],
-  ['The Watchers', '2024', 'https://letterboxd.com/tinyhomecinema/film/the-watchers-2024/', 'https://a.ltrbxd.com/resized/film-poster/9/7/9/7/8/3/979783-the-watchers-0-600-0-900-crop.jpg'],
 ]
