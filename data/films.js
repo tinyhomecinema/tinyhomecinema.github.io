f28aa139693e352ed7a47063f480fc98e60c9462
Cineminha <tinyhomecinema@gmail.com>
2024-12-28 21:00:48 -0300

    [auto] Updated data

--- data/films.js
+++ data/films.js
@@ -1,4 +1,5 @@
 var films = [
+  ['The Boy and the Heron', '2023', 'https://letterboxd.com/tinyhomecinema/film/the-boy-and-the-heron/', 'https://a.ltrbxd.com/resized/film-poster/4/3/8/6/9/2/438692-the-boy-and-the-heron-0-600-0-900-crop.jpg'],
   ['Juror #2', '2024', 'https://letterboxd.com/tinyhomecinema/film/juror-2/1/', 'https://a.ltrbxd.com/resized/film-poster/9/9/7/7/2/2/997722-juror-2-0-600-0-900-crop.jpg'],
   ['Fly Me to the Moon', '2024', 'https://letterboxd.com/tinyhomecinema/film/fly-me-to-the-moon-2024/', 'https://a.ltrbxd.com/resized/film-poster/8/6/1/0/3/9/861039-fly-me-to-the-moon-0-600-0-900-crop.jpg'],
   ['Asteroid City', '2023', 'https://letterboxd.com/tinyhomecinema/film/asteroid-city/', 'https://a.ltrbxd.com/resized/film-poster/6/6/8/5/5/5/668555-asteroid-city-0-600-0-900-crop.jpg'],
@@ -8,5 +9,4 @@ var films = [
   ['Ghostbusters: Frozen Empire', '2024', 'https://letterboxd.com/tinyhomecinema/film/ghostbusters-frozen-empire/', 'https://a.ltrbxd.com/resized/film-poster/8/7/1/0/4/1/871041-ghostbusters-frozen-empire-0-600-0-900-crop.jpg'],
   ['Monkey Man', '2024', 'https://letterboxd.com/tinyhomecinema/film/monkey-man/', 'https://a.ltrbxd.com/resized/film-poster/4/8/8/7/5/1/488751-monkey-man-0-600-0-900-crop.jpg'],
   ['Caddo Lake', '2024', 'https://letterboxd.com/tinyhomecinema/film/caddo-lake/', 'https://a.ltrbxd.com/resized/film-poster/7/7/7/9/7/9/777979-caddo-lake-0-600-0-900-crop.jpg'],
-  ['Late Night with the Devil', '2023', 'https://letterboxd.com/tinyhomecinema/film/late-night-with-the-devil/', 'https://a.ltrbxd.com/resized/film-poster/8/4/3/4/1/5/843415-late-night-with-the-devil-0-600-0-900-crop.jpg'],
 ]
