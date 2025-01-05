6a59143a4b0d5f3fd1af509d72ffdd9e2024a5c1
Cineminha <tinyhomecinema@gmail.com>
2025-01-05 19:00:31 -0300

    [auto] Updated data

--- data/films.js
+++ data/films.js
@@ -1,4 +1,5 @@
 var films = [
+  ['Wallace &amp; Gromit: Vengeance Most Fowl', '2024', 'https://letterboxd.com/tinyhomecinema/film/wallace-gromit-vengeance-most-fowl/', 'https://a.ltrbxd.com/resized/film-poster/8/3/4/5/6/1/834561-wallace-gromit-vengeance-most-fowl-0-600-0-900-crop.jpg'],
   ['The Boy and the Heron', '2023', 'https://letterboxd.com/tinyhomecinema/film/the-boy-and-the-heron/', 'https://a.ltrbxd.com/resized/film-poster/4/3/8/6/9/2/438692-the-boy-and-the-heron-0-600-0-900-crop.jpg'],
   ['Juror #2', '2024', 'https://letterboxd.com/tinyhomecinema/film/juror-2/1/', 'https://a.ltrbxd.com/resized/film-poster/9/9/7/7/2/2/997722-juror-2-0-600-0-900-crop.jpg'],
   ['Fly Me to the Moon', '2024', 'https://letterboxd.com/tinyhomecinema/film/fly-me-to-the-moon-2024/', 'https://a.ltrbxd.com/resized/film-poster/8/6/1/0/3/9/861039-fly-me-to-the-moon-0-600-0-900-crop.jpg'],
@@ -8,5 +9,4 @@ var films = [
   ['A Mother\'s Embrace', '2024', 'https://letterboxd.com/tinyhomecinema/film/a-mothers-embrace/', 'https://a.ltrbxd.com/resized/film-poster/9/5/0/9/3/3/950933-a-mothers-embrace-0-600-0-900-crop.jpg'],
   ['Ghostbusters: Frozen Empire', '2024', 'https://letterboxd.com/tinyhomecinema/film/ghostbusters-frozen-empire/', 'https://a.ltrbxd.com/resized/film-poster/8/7/1/0/4/1/871041-ghostbusters-frozen-empire-0-600-0-900-crop.jpg'],
   ['Monkey Man', '2024', 'https://letterboxd.com/tinyhomecinema/film/monkey-man/', 'https://a.ltrbxd.com/resized/film-poster/4/8/8/7/5/1/488751-monkey-man-0-600-0-900-crop.jpg'],
-  ['Caddo Lake', '2024', 'https://letterboxd.com/tinyhomecinema/film/caddo-lake/', 'https://a.ltrbxd.com/resized/film-poster/7/7/7/9/7/9/777979-caddo-lake-0-600-0-900-crop.jpg'],
 ]
