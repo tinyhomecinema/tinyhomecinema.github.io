e3794c2e99c96bc2fe1a489e6fda0b1df2acd204
Cineminha <tinyhomecinema@gmail.com>
2025-05-03 11:46:14 -0300

    Moved MUBI from Past to Current Services

--- data/services.js
+++ data/services.js
@@ -5,7 +5,7 @@ var services = [
   ['Apple TV+', 'https://letterboxd.com/tinyhomecinema/tag/apple-tv-/films/', 'current'],
   ['Globoplay', 'https://letterboxd.com/tinyhomecinema/tag/globoplay/films/', 'current'],
   ['Filmicca', 'https://letterboxd.com/tinyhomecinema/tag/filmicca/films/', 'current'],
-  ['MUBI', 'https://letterboxd.com/tinyhomecinema/tag/mubi/films/', 'past'],
+  ['MUBI', 'https://letterboxd.com/tinyhomecinema/tag/mubi/films/', 'current'],
   ['Belas Artes', 'https://letterboxd.com/tinyhomecinema/tag/belas-artes/films/', 'past'],
   ['Telecine', 'https://letterboxd.com/tinyhomecinema/tag/telecine/films/', 'past'],
   ['Disney+', 'https://letterboxd.com/tinyhomecinema/tag/disney-/films/', 'past'],
