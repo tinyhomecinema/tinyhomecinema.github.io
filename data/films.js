d81fe1ccb5e10939cd4db383834d51459fcb2144
Cineminha <tinyhomecinema@gmail.com>
2025-05-23 02:05:53 -0300

    [auto] Updated data

---
+++ data/films.js
@@ -0,0 +1,12 @@
+var films = [
+  ['Longlegs', '2024', 'https://letterboxd.com/tinyhomecinema/film/longlegs/', 'https://a.ltrbxd.com/resized/film-poster/1/1/1/0/0/5/9/1110059-longlegs-0-600-0-900-crop.jpg'],
+  ['A Different Man', '2024', 'https://letterboxd.com/tinyhomecinema/film/a-different-man/', 'https://a.ltrbxd.com/resized/film-poster/8/9/1/2/9/1/891291-a-different-man-0-600-0-900-crop.jpg'],
+  ['Conclave', '2024', 'https://letterboxd.com/tinyhomecinema/film/conclave/', 'https://a.ltrbxd.com/resized/film-poster/8/7/7/2/5/3/877253-conclave-0-600-0-900-crop.jpg'],
+  ['My Penguin Friend', '2024', 'https://letterboxd.com/tinyhomecinema/film/my-penguin-friend/', 'https://a.ltrbxd.com/resized/film-poster/1/0/4/5/4/5/4/1045454-my-penguin-friend-0-600-0-900-crop.jpg'],
+  ['The Wild Robot', '2024', 'https://letterboxd.com/tinyhomecinema/film/the-wild-robot/', 'https://a.ltrbxd.com/resized/film-poster/1/0/7/1/1/9/6/1071196-the-wild-robot-0-600-0-900-crop.jpg'],
+  ['Chuck Billy and The Marvelous Guava Tree', '2024', 'https://letterboxd.com/tinyhomecinema/film/chuck-billy-and-the-marvelous-guava-tree/', 'https://a.ltrbxd.com/resized/film-poster/8/4/0/7/9/5/840795-chico-bento-e-a-goiabeira-maraviosa-0-600-0-900-crop.jpg'],
+  ['The Gorge', '2025', 'https://letterboxd.com/tinyhomecinema/film/the-gorge-2025/', 'https://a.ltrbxd.com/resized/film-poster/8/5/4/6/8/7/854687-the-gorge-2025-0-600-0-900-crop.jpg'],
+  ['The Girl with the Needle', '2024', 'https://letterboxd.com/tinyhomecinema/film/the-girl-with-the-needle/', 'https://a.ltrbxd.com/resized/film-poster/1/1/1/6/3/2/1/1116321-the-girl-with-the-needle-0-600-0-900-crop.jpg'],
+  ['The Substance', '2024', 'https://letterboxd.com/tinyhomecinema/film/the-substance/', 'https://a.ltrbxd.com/resized/film-poster/8/3/8/1/4/0/838140-the-substance-0-600-0-900-crop.jpg'],
+  ['MaXXXine', '2024', 'https://letterboxd.com/tinyhomecinema/film/maxxxine/', 'https://a.ltrbxd.com/resized/film-poster/9/2/2/8/5/8/922858-maxxxine-0-600-0-900-crop.jpg'],
+]
