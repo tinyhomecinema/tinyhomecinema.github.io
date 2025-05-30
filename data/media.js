829b3388658a158f42b22cbce5359b18f53c2c09
Cineminha <tinyhomecinema@gmail.com>
2025-05-30 16:45:16 -0300

    [auto] Updated data

--- data/media.js
+++ data/media.js
@@ -1,6 +1,6 @@
 var shelves = [
   ['Movies', 415, 'https://letterboxd.com/tinyhomecinema/list/movies-shelf/', 'shelf'],
-  ['TV Shows', 18, 'https://trakt.tv/users/cineminha_shelf/lists/tv-show-collection', 'shelf'],
+  ['TV Shows', 415, 'https://letterboxd.com/tinyhomecinema/list/movies-shelf/', 'shelf'],
   ['Music', 35, 'https://www.librarything.com/catalog.php?view=cineminha&collection=898678&shelf=shelf&sort=stampREV', 'shelf'],
   ['Books', 27, 'https://www.librarything.com/catalog.php?view=cineminha&collection=898806&shelf=shelf', 'shelf']
 ]
