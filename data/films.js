5a5aa172eace88aa0248e4e9773aea46b7f108c5
Cineminha <tinyhomecinema@gmail.com>
2025-05-31 20:00:21 -0300

    [auto] Updated data

--- data/films.js
+++ data/films.js
@@ -1,4 +1,5 @@
 var films = [
+  ['Heretic', '2024', 'https://letterboxd.com/tinyhomecinema/film/heretic-2024/', 'https://a.ltrbxd.com/resized/film-poster/1/0/2/5/3/3/1/1025331-heretic-2024-0-600-0-900-crop.jpg'],
   ['Longlegs', '2024', 'https://letterboxd.com/tinyhomecinema/film/longlegs/', 'https://a.ltrbxd.com/resized/film-poster/1/1/1/0/0/5/9/1110059-longlegs-0-600-0-900-crop.jpg'],
   ['A Different Man', '2024', 'https://letterboxd.com/tinyhomecinema/film/a-different-man/', 'https://a.ltrbxd.com/resized/film-poster/8/9/1/2/9/1/891291-a-different-man-0-600-0-900-crop.jpg'],
   ['Conclave', '2024', 'https://letterboxd.com/tinyhomecinema/film/conclave/', 'https://a.ltrbxd.com/resized/film-poster/8/7/7/2/5/3/877253-conclave-0-600-0-900-crop.jpg'],
@@ -8,5 +9,4 @@ var films = [
   ['The Gorge', '2025', 'https://letterboxd.com/tinyhomecinema/film/the-gorge-2025/', 'https://a.ltrbxd.com/resized/film-poster/8/5/4/6/8/7/854687-the-gorge-2025-0-600-0-900-crop.jpg'],
   ['The Girl with the Needle', '2024', 'https://letterboxd.com/tinyhomecinema/film/the-girl-with-the-needle/', 'https://a.ltrbxd.com/resized/film-poster/1/1/1/6/3/2/1/1116321-the-girl-with-the-needle-0-600-0-900-crop.jpg'],
   ['The Substance', '2024', 'https://letterboxd.com/tinyhomecinema/film/the-substance/', 'https://a.ltrbxd.com/resized/film-poster/8/3/8/1/4/0/838140-the-substance-0-600-0-900-crop.jpg'],
-  ['MaXXXine', '2024', 'https://letterboxd.com/tinyhomecinema/film/maxxxine/', 'https://a.ltrbxd.com/resized/film-poster/9/2/2/8/5/8/922858-maxxxine-0-600-0-900-crop.jpg'],
 ]
