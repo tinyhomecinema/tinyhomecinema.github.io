b1d1b64041a8a507bbf20432367afe783eba18a3
Cineminha <tinyhomecinema@gmail.com>
2025-06-08 18:45:19 -0300

    [auto] Updated data

--- data/films.js
+++ data/films.js
@@ -1,4 +1,5 @@
 var films = [
+  ['Blink Twice', '2024', 'https://letterboxd.com/tinyhomecinema/film/blink-twice/', 'https://a.ltrbxd.com/resized/film-poster/7/5/6/0/4/2/756042-blink-twice-0-600-0-900-crop.jpg'],
   ['The Assessment', '2024', 'https://letterboxd.com/tinyhomecinema/film/the-assessment-2024/', 'https://a.ltrbxd.com/resized/film-poster/1/1/9/7/8/8/4/1197884-the-assessment-2024-0-600-0-900-crop.jpg'],
   ['Heretic', '2024', 'https://letterboxd.com/tinyhomecinema/film/heretic-2024/', 'https://a.ltrbxd.com/resized/film-poster/1/0/2/5/3/3/1/1025331-heretic-2024-0-600-0-900-crop.jpg'],
   ['Longlegs', '2024', 'https://letterboxd.com/tinyhomecinema/film/longlegs/', 'https://a.ltrbxd.com/resized/film-poster/1/1/1/0/0/5/9/1110059-longlegs-0-600-0-900-crop.jpg'],
@@ -8,5 +9,4 @@ var films = [
   ['The Wild Robot', '2024', 'https://letterboxd.com/tinyhomecinema/film/the-wild-robot/', 'https://a.ltrbxd.com/resized/film-poster/1/0/7/1/1/9/6/1071196-the-wild-robot-0-600-0-900-crop.jpg'],
   ['Chuck Billy and The Marvelous Guava Tree', '2024', 'https://letterboxd.com/tinyhomecinema/film/chuck-billy-and-the-marvelous-guava-tree/', 'https://a.ltrbxd.com/resized/film-poster/8/4/0/7/9/5/840795-chuck-billy-and-the-marvelous-guava-tree-0-600-0-900-crop.jpg'],
   ['The Gorge', '2025', 'https://letterboxd.com/tinyhomecinema/film/the-gorge-2025/', 'https://a.ltrbxd.com/resized/film-poster/8/5/4/6/8/7/854687-the-gorge-2025-0-600-0-900-crop.jpg'],
-  ['The Girl with the Needle', '2024', 'https://letterboxd.com/tinyhomecinema/film/the-girl-with-the-needle/', 'https://a.ltrbxd.com/resized/film-poster/1/1/1/6/3/2/1/1116321-the-girl-with-the-needle-0-600-0-900-crop.jpg'],
 ]
