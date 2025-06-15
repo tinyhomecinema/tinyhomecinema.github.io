5ac757cc110fc26ad01bf4498fb6fa1e24fd3cc4
Cineminha <tinyhomecinema@gmail.com>
2025-06-15 20:11:16 -0300

    [auto] Updated data

--- data/films.js
+++ data/films.js
@@ -1,4 +1,5 @@
 var films = [
+  ['Echo Valley', '2025', 'https://letterboxd.com/tinyhomecinema/film/echo-valley/', 'https://a.ltrbxd.com/resized/film-poster/9/8/9/1/2/2/989122-echo-valley-0-600-0-900-crop.jpg'],
   ['Blink Twice', '2024', 'https://letterboxd.com/tinyhomecinema/film/blink-twice/', 'https://a.ltrbxd.com/resized/film-poster/7/5/6/0/4/2/756042-blink-twice-0-600-0-900-crop.jpg'],
   ['The Assessment', '2024', 'https://letterboxd.com/tinyhomecinema/film/the-assessment-2024/', 'https://a.ltrbxd.com/resized/film-poster/1/1/9/7/8/8/4/1197884-the-assessment-2024-0-600-0-900-crop.jpg'],
   ['Heretic', '2024', 'https://letterboxd.com/tinyhomecinema/film/heretic-2024/', 'https://a.ltrbxd.com/resized/film-poster/1/0/2/5/3/3/1/1025331-heretic-2024-0-600-0-900-crop.jpg'],
@@ -8,5 +9,4 @@ var films = [
   ['My Penguin Friend', '2024', 'https://letterboxd.com/tinyhomecinema/film/my-penguin-friend/', 'https://a.ltrbxd.com/resized/film-poster/1/0/4/5/4/5/4/1045454-my-penguin-friend-0-600-0-900-crop.jpg'],
   ['The Wild Robot', '2024', 'https://letterboxd.com/tinyhomecinema/film/the-wild-robot/', 'https://a.ltrbxd.com/resized/film-poster/1/0/7/1/1/9/6/1071196-the-wild-robot-0-600-0-900-crop.jpg'],
   ['Chuck Billy and The Marvelous Guava Tree', '2024', 'https://letterboxd.com/tinyhomecinema/film/chuck-billy-and-the-marvelous-guava-tree/', 'https://a.ltrbxd.com/resized/film-poster/8/4/0/7/9/5/840795-chuck-billy-and-the-marvelous-guava-tree-0-600-0-900-crop.jpg'],
-  ['The Gorge', '2025', 'https://letterboxd.com/tinyhomecinema/film/the-gorge-2025/', 'https://a.ltrbxd.com/resized/film-poster/8/5/4/6/8/7/854687-the-gorge-2025-0-600-0-900-crop.jpg'],
 ]
