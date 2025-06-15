9698eccf1a3ec9bb4de2123fc31cced47d6de0b2
Cineminha <tinyhomecinema@gmail.com>
2025-06-15 15:33:30 -0300

    [auto] Updated data

--- data/media.js
+++ data/media.js
@@ -6,19 +6,19 @@ var shelves = [
 ]
 
 var collection = [
-  ['Films', 345, 'https://letterboxd.com/tinyhomecinema/list/film-collection/', 'collection'],
-  ['Miniseries', 345, 'https://letterboxd.com/tinyhomecinema/list/film-collection/', 'collection'],
-  ['Shorts', 345, 'https://letterboxd.com/tinyhomecinema/list/film-collection/', 'collection'],
-  ['Documentaries', 19, 'https://letterboxd.com/tinyhomecinema/list/film-collection/', 'collection'],
-  ['Blu-ray', 19, 'https://letterboxd.com/tinyhomecinema/list/film-collection/', 'media'],
-  ['DVD', 19, 'https://letterboxd.com/tinyhomecinema/list/film-collection/', 'media'],
-  ['VHS', 19, 'https://letterboxd.com/tinyhomecinema/list/film-collection/', 'media'],
-  ['Dolby Atmos', 19, 'https://letterboxd.com/tinyhomecinema/list/film-collection/', 'audio'],
-  ['DTS X', 19, 'https://letterboxd.com/tinyhomecinema/list/film-collection/', 'audio'],
+  ['Films', 27, 'https://www.librarything.com/catalog.php?view=cineminha&collection=898806&shelf=shelf', 'collection'],
+  ['Miniseries', 6, 'https://letterboxd.com/tinyhomecinema/list/miniseries-collection/', 'collection'],
+  ['Shorts', 6, 'https://letterboxd.com/tinyhomecinema/list/miniseries-collection/', 'collection'],
+  ['Documentaries', 19, 'https://letterboxd.com/tinyhomecinema/list/miniseries-collection/', 'collection'],
+  ['Blu-ray', 19, 'https://letterboxd.com/tinyhomecinema/list/miniseries-collection/', 'media'],
+  ['DVD', 19, 'https://letterboxd.com/tinyhomecinema/list/miniseries-collection/', 'media'],
+  ['VHS', 19, 'https://letterboxd.com/tinyhomecinema/list/miniseries-collection/', 'media'],
+  ['Dolby Atmos', 19, 'https://letterboxd.com/tinyhomecinema/list/miniseries-collection/', 'audio'],
+  ['DTS X', 19, 'https://letterboxd.com/tinyhomecinema/list/miniseries-collection/', 'audio'],
 ]
 
 var tv_collection = [
-  ['Blu-ray', 19, 'https://letterboxd.com/tinyhomecinema/list/film-collection/', 'media'],
-  ['DVD', 19, 'https://letterboxd.com/tinyhomecinema/list/film-collection/', 'media'],
-  ['Dolby Atmos', 19, 'https://letterboxd.com/tinyhomecinema/list/film-collection/', 'audio'],
+  ['Blu-ray', 19, 'https://letterboxd.com/tinyhomecinema/list/miniseries-collection/', 'media'],
+  ['DVD', 19, 'https://letterboxd.com/tinyhomecinema/list/miniseries-collection/', 'media'],
+  ['Dolby Atmos', 19, 'https://letterboxd.com/tinyhomecinema/list/miniseries-collection/', 'audio'],
 ]
