f4b5599978732e0369c29abd9ce45613eca5edcd
Cineminha <tinyhomecinema@gmail.com>
2025-06-16 14:16:54 -0300

    [auto] Updated data

--- data/media.js
+++ data/media.js
@@ -1,6 +1,6 @@
 var shelves = [
   ['Movies', 415, 'https://letterboxd.com/tinyhomecinema/list/movies-shelf/', 'shelf'],
-  ['TV Shows', 415, 'https://letterboxd.com/tinyhomecinema/list/movies-shelf/', 'shelf'],
+  ['TV Shows', 18, 'https://trakt.tv/users/cineminha_shelf/lists/tv-show-collection', 'shelf'],
   ['Music', 35, 'https://www.librarything.com/catalog.php?view=cineminha&collection=898678&shelf=shelf&sort=stampREV', 'shelf'],
   ['Books', 27, 'https://www.librarything.com/catalog.php?view=cineminha&collection=898806&shelf=shelf', 'shelf']
 ]
@@ -8,7 +8,7 @@ var shelves = [
 var collection = [
   ['Films', 345, 'https://letterboxd.com/tinyhomecinema/list/film-collection/', 'collection'],
   ['Miniseries', 6, 'https://letterboxd.com/tinyhomecinema/list/miniseries-collection/', 'collection'],
-  ['Shorts', 6, 'https://letterboxd.com/tinyhomecinema/list/miniseries-collection/', 'collection'],
+  ['Shorts', 68, 'https://letterboxd.com/tinyhomecinema/list/short-films/', 'collection'],
   ['Documentaries', 19, 'https://letterboxd.com/tinyhomecinema/list/special-feature-documentaries/', 'collection'],
   ['Blu-ray', 151, 'https://letterboxd.com/tinyhomecinema/list/blu-ray/', 'media'],
   ['DVD', 239, 'https://letterboxd.com/tinyhomecinema/list/dvd/', 'media'],
