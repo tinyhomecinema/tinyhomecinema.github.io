34794b53a53cfe9015bed213d47e62168d43c0ce
Cineminha <tinyhomecinema@gmail.com>
2025-06-15 22:07:24 -0300

    [auto] Updated data

--- data/media.js
+++ data/media.js
@@ -1,24 +1,24 @@
 var shelves = [
-  ['Movies', 415, 'https://letterboxd.com/tinyhomecinema/list/movies-shelf/', 'shelf'],
-  ['TV Shows', 415, 'https://letterboxd.com/tinyhomecinema/list/movies-shelf/', 'shelf'],
+  ['Movies', , '', 'shelf'],
+  ['TV Shows', 18, 'https://trakt.tv/users/cineminha_shelf/lists/tv-show-collection', 'shelf'],
   ['Music', 35, 'https://www.librarything.com/catalog.php?view=cineminha&collection=898678&shelf=shelf&sort=stampREV', 'shelf'],
   ['Books', 27, 'https://www.librarything.com/catalog.php?view=cineminha&collection=898806&shelf=shelf', 'shelf']
 ]
 
 var collection = [
   ['Films', 345, 'https://letterboxd.com/tinyhomecinema/list/film-collection/', 'collection'],
-  ['Miniseries', 345, 'https://letterboxd.com/tinyhomecinema/list/film-collection/', 'collection'],
+  ['Miniseries', 6, 'https://letterboxd.com/tinyhomecinema/list/miniseries-collection/', 'collection'],
   ['Shorts', 68, 'https://letterboxd.com/tinyhomecinema/list/short-films/', 'collection'],
   ['Documentaries', 19, 'https://letterboxd.com/tinyhomecinema/list/special-feature-documentaries/', 'collection'],
   ['Blu-ray', 151, 'https://letterboxd.com/tinyhomecinema/list/blu-ray/', 'media'],
-  ['DVD', 239, 'https://letterboxd.com/tinyhomecinema/list/dvd/', 'media'],
+  ['DVD', 151, 'https://letterboxd.com/tinyhomecinema/list/blu-ray/', 'media'],
   ['VHS', 5, 'https://letterboxd.com/tinyhomecinema/list/decor-vhs-tapes/', 'media'],
-  ['Dolby Atmos', 27, 'https://letterboxd.com/tinyhomecinema/list/dolby-atmos/', 'audio'],
-  ['DTS X', 13, 'https://letterboxd.com/tinyhomecinema/list/dts-x/', 'audio'],
+  ['Dolby Atmos', 5, 'https://letterboxd.com/tinyhomecinema/list/decor-vhs-tapes/', 'audio'],
+  ['DTS X', 5, 'https://letterboxd.com/tinyhomecinema/list/decor-vhs-tapes/', 'audio'],
 ]
 
 var tv_collection = [
-  ['Blu-ray', 16, 'https://trakt.tv/users/cineminha_shelf/lists/tv-show-blu-rays', 'media'],
+  ['Blu-ray', 5, 'https://letterboxd.com/tinyhomecinema/list/decor-vhs-tapes/', 'media'],
   ['DVD', 3, 'https://trakt.tv/users/cineminha_shelf/lists/tv-show-dvds', 'media'],
   ['Dolby Atmos', 3, 'https://trakt.tv/users/cineminha_shelf/lists/tv-show-dvds', 'audio'],
 ]
