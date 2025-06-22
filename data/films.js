c9444c3dfe4e95b7917b4d8084ac7306d4ef88de
Cineminha <tinyhomecinema@gmail.com>
2025-06-22 02:17:46 -0300

    [auto] Updated data

--- data/films.js
+++ data/films.js
@@ -1,12 +1,12 @@
 var films = [
+  ['MaXXXine', '2024', 'https://letterboxd.com/tinyhomecinema/film/maxxxine/', 'https://a.ltrbxd.com/resized/film-poster/9/2/2/8/5/8/922858-maxxxine-0-600-0-900-crop.jpg'],
+  ['Fancy Dance', '2023', 'https://letterboxd.com/tinyhomecinema/film/fancy-dance-2023/', 'https://a.ltrbxd.com/resized/film-poster/9/5/4/4/6/8/954468-fancy-dance-0-600-0-900-crop.jpg'],
+  ['Wallace &amp; Gromit: Vengeance Most Fowl', '2024', 'https://letterboxd.com/tinyhomecinema/film/wallace-gromit-vengeance-most-fowl/', 'https://a.ltrbxd.com/resized/film-poster/8/3/4/5/6/1/834561-wallace-gromit-vengeance-most-fowl-0-600-0-900-crop.jpg'],
+  ['The Boy and the Heron', '2023', 'https://letterboxd.com/tinyhomecinema/film/the-boy-and-the-heron/', 'https://a.ltrbxd.com/resized/film-poster/4/3/8/6/9/2/438692-the-boy-and-the-heron-0-600-0-900-crop.jpg'],
+  ['Juror #2', '2024', 'https://letterboxd.com/tinyhomecinema/film/juror-2/1/', 'https://a.ltrbxd.com/resized/film-poster/9/9/7/7/2/2/997722-juror-2-0-600-0-900-crop.jpg'],
+  ['Fly Me to the Moon', '2024', 'https://letterboxd.com/tinyhomecinema/film/fly-me-to-the-moon-2024/', 'https://a.ltrbxd.com/resized/film-poster/8/6/1/0/3/9/861039-fly-me-to-the-moon-0-600-0-900-crop.jpg'],
   ['Warfare', '2025', 'https://letterboxd.com/tinyhomecinema/film/warfare/', 'https://a.ltrbxd.com/resized/film-poster/1/1/2/4/7/0/1/1124701-warfare-0-600-0-900-crop.jpg'],
   ['Echo Valley', '2025', 'https://letterboxd.com/tinyhomecinema/film/echo-valley/', 'https://a.ltrbxd.com/resized/film-poster/9/8/9/1/2/2/989122-echo-valley-0-600-0-900-crop.jpg'],
   ['Blink Twice', '2024', 'https://letterboxd.com/tinyhomecinema/film/blink-twice/', 'https://a.ltrbxd.com/resized/film-poster/7/5/6/0/4/2/756042-blink-twice-0-600-0-900-crop.jpg'],
   ['The Assessment', '2024', 'https://letterboxd.com/tinyhomecinema/film/the-assessment-2024/', 'https://a.ltrbxd.com/resized/film-poster/1/1/9/7/8/8/4/1197884-the-assessment-2024-0-600-0-900-crop.jpg'],
-  ['Heretic', '2024', 'https://letterboxd.com/tinyhomecinema/film/heretic-2024/', 'https://a.ltrbxd.com/resized/film-poster/1/0/2/5/3/3/1/1025331-heretic-2024-0-600-0-900-crop.jpg'],
-  ['Longlegs', '2024', 'https://letterboxd.com/tinyhomecinema/film/longlegs/', 'https://a.ltrbxd.com/resized/film-poster/1/1/1/0/0/5/9/1110059-longlegs-0-600-0-900-crop.jpg'],
-  ['A Different Man', '2024', 'https://letterboxd.com/tinyhomecinema/film/a-different-man/', 'https://a.ltrbxd.com/resized/film-poster/8/9/1/2/9/1/891291-a-different-man-0-600-0-900-crop.jpg'],
-  ['Conclave', '2024', 'https://letterboxd.com/tinyhomecinema/film/conclave/', 'https://a.ltrbxd.com/resized/film-poster/8/7/7/2/5/3/877253-conclave-0-600-0-900-crop.jpg'],
-  ['My Penguin Friend', '2024', 'https://letterboxd.com/tinyhomecinema/film/my-penguin-friend/', 'https://a.ltrbxd.com/resized/film-poster/1/0/4/5/4/5/4/1045454-my-penguin-friend-0-600-0-900-crop.jpg'],
-  ['The Wild Robot', '2024', 'https://letterboxd.com/tinyhomecinema/film/the-wild-robot/', 'https://a.ltrbxd.com/resized/film-poster/1/0/7/1/1/9/6/1071196-the-wild-robot-0-600-0-900-crop.jpg'],
 ]
