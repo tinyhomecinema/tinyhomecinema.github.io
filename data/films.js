1ca21a3fb2f9f00f2a2b691c07ea22dd5e5a6206
Cineminha <tinyhomecinema@gmail.com>
2025-06-22 11:45:18 -0300

    [auto] Updated data

--- data/films.js
+++ data/films.js
@@ -1,4 +1,5 @@
 var films = [
+  ['Warfare', '2025', 'https://letterboxd.com/tinyhomecinema/film/warfare/', 'https://a.ltrbxd.com/resized/film-poster/1/1/2/4/7/0/1/1124701-warfare-0-600-0-900-crop.jpg'],
   ['Echo Valley', '2025', 'https://letterboxd.com/tinyhomecinema/film/echo-valley/', 'https://a.ltrbxd.com/resized/film-poster/9/8/9/1/2/2/989122-echo-valley-0-600-0-900-crop.jpg'],
   ['Blink Twice', '2024', 'https://letterboxd.com/tinyhomecinema/film/blink-twice/', 'https://a.ltrbxd.com/resized/film-poster/7/5/6/0/4/2/756042-blink-twice-0-600-0-900-crop.jpg'],
   ['The Assessment', '2024', 'https://letterboxd.com/tinyhomecinema/film/the-assessment-2024/', 'https://a.ltrbxd.com/resized/film-poster/1/1/9/7/8/8/4/1197884-the-assessment-2024-0-600-0-900-crop.jpg'],
