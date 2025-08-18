2a126f9cf03cfdd59f1a6a8622acc146ed4ed30f
Cineminha <tinyhomecinema@gmail.com>
2025-08-18 14:18:41 -0300

    [auto] Updated data

--- data/media.js
+++ data/media.js
@@ -6,7 +6,7 @@ var shelves = [
 ]
 
 var collection = [
-  ['Films', 345, 'https://letterboxd.com/tinyhomecinema/list/film-collection/', 'collection'],
+  ['Films', 27, 'https://www.librarything.com/catalog.php?view=cineminha&collection=898806&shelf=shelf', 'collection'],
   ['Miniseries', 6, 'https://letterboxd.com/tinyhomecinema/list/miniseries-collection/', 'collection'],
   ['Shorts', 68, 'https://letterboxd.com/tinyhomecinema/list/short-films/', 'collection'],
   ['Documentaries', 19, 'https://letterboxd.com/tinyhomecinema/list/special-feature-documentaries/', 'collection'],
@@ -14,7 +14,7 @@ var collection = [
   ['DVD', 239, 'https://letterboxd.com/tinyhomecinema/list/dvd/', 'media'],
   ['VHS', 5, 'https://letterboxd.com/tinyhomecinema/list/decor-vhs-tapes/', 'media'],
   ['Dolby Atmos', 28, 'https://letterboxd.com/tinyhomecinema/list/dolby-atmos/', 'audio'],
-  ['DTS X', 13, 'https://letterboxd.com/tinyhomecinema/list/dts-x/', 'audio'],
+  ['DTS X', 28, 'https://letterboxd.com/tinyhomecinema/list/dolby-atmos/', 'audio'],
 ]
 
 var tv_collection = [
