1bb426cb5f16748923130f77fe27a0a458e70692
Cineminha <tinyhomecinema@gmail.com>
2025-11-18 10:15:15 -0300

    [auto] Updated data

--- data/media.js
+++ data/media.js
@@ -7,18 +7,18 @@ var shelves = [
 
 var collection = [
   ['Films', 27, 'https://www.librarything.com/catalog.php?view=cineminha&collection=898806&shelf=shelf', 'collection'],
-  ['Miniseries', 6, 'https://letterboxd.com/tinyhomecinema/list/miniseries-collection/', 'collection'],
-  ['Shorts', 6, 'https://letterboxd.com/tinyhomecinema/list/miniseries-collection/', 'collection'],
-  ['Documentaries', 6, 'https://letterboxd.com/tinyhomecinema/list/miniseries-collection/', 'collection'],
-  ['Blu-ray', 6, 'https://letterboxd.com/tinyhomecinema/list/miniseries-collection/', 'media'],
-  ['DVD', 6, 'https://letterboxd.com/tinyhomecinema/list/miniseries-collection/', 'media'],
-  ['VHS', 6, 'https://letterboxd.com/tinyhomecinema/list/miniseries-collection/', 'media'],
-  ['Dolby Atmos', 6, 'https://letterboxd.com/tinyhomecinema/list/miniseries-collection/', 'audio'],
-  ['DTS X', 6, 'https://letterboxd.com/tinyhomecinema/list/miniseries-collection/', 'audio'],
+  ['Miniseries', 27, 'https://www.librarything.com/catalog.php?view=cineminha&collection=898806&shelf=shelf', 'collection'],
+  ['Shorts', 27, 'https://www.librarything.com/catalog.php?view=cineminha&collection=898806&shelf=shelf', 'collection'],
+  ['Documentaries', 27, 'https://www.librarything.com/catalog.php?view=cineminha&collection=898806&shelf=shelf', 'collection'],
+  ['Blu-ray', 27, 'https://www.librarything.com/catalog.php?view=cineminha&collection=898806&shelf=shelf', 'media'],
+  ['DVD', 27, 'https://www.librarything.com/catalog.php?view=cineminha&collection=898806&shelf=shelf', 'media'],
+  ['VHS', 27, 'https://www.librarything.com/catalog.php?view=cineminha&collection=898806&shelf=shelf', 'media'],
+  ['Dolby Atmos', 27, 'https://www.librarything.com/catalog.php?view=cineminha&collection=898806&shelf=shelf', 'audio'],
+  ['DTS X', 27, 'https://www.librarything.com/catalog.php?view=cineminha&collection=898806&shelf=shelf', 'audio'],
 ]
 
 var tv_collection = [
-  ['Blu-ray', 6, 'https://letterboxd.com/tinyhomecinema/list/miniseries-collection/', 'media'],
-  ['DVD', 6, 'https://letterboxd.com/tinyhomecinema/list/miniseries-collection/', 'media'],
-  ['Dolby Atmos', 6, 'https://letterboxd.com/tinyhomecinema/list/miniseries-collection/', 'audio'],
+  ['Blu-ray', 27, 'https://www.librarything.com/catalog.php?view=cineminha&collection=898806&shelf=shelf', 'media'],
+  ['DVD', 27, 'https://www.librarything.com/catalog.php?view=cineminha&collection=898806&shelf=shelf', 'media'],
+  ['Dolby Atmos', 27, 'https://www.librarything.com/catalog.php?view=cineminha&collection=898806&shelf=shelf', 'audio'],
 ]
