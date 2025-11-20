8d53e36c37f9439281dac78b3cad46ad27f5a58e
Cineminha <tinyhomecinema@gmail.com>
2025-11-20 18:45:25 -0300

    [auto] Updated data

--- data/films.js
+++ data/films.js
@@ -1,4 +1,5 @@
 var films = [
+  ['Frankenstein', '2025', 'https://letterboxd.com/tinyhomecinema/film/frankenstein-2025/', 'https://a.ltrbxd.com/resized/film-poster/9/5/8/1/0/0/958100-frankenstein-2025-0-600-0-900-crop.jpg'],
   ['Superman', '2025', 'https://letterboxd.com/tinyhomecinema/film/superman-2025/', 'https://a.ltrbxd.com/resized/film-poster/9/5/7/0/5/0/957050-superman-2025-0-600-0-900-crop.jpg'],
   ['The Shadow Strays', '2024', 'https://letterboxd.com/tinyhomecinema/film/the-shadow-strays/', 'https://a.ltrbxd.com/resized/film-poster/1/0/3/8/2/1/8/1038218-the-shadow-strays-0-600-0-900-crop.jpg'],
   ['How to Make Millions Before Grandma Dies', '2024', 'https://letterboxd.com/tinyhomecinema/film/how-to-make-millions-before-grandma-dies/', 'https://a.ltrbxd.com/resized/film-poster/9/9/5/2/7/4/995274-how-to-make-millions-before-grandma-dies-0-600-0-900-crop.jpg'],
@@ -8,5 +9,4 @@ var films = [
   ['Blink Twice', '2024', 'https://letterboxd.com/tinyhomecinema/film/blink-twice/', 'https://a.ltrbxd.com/resized/film-poster/7/5/6/0/4/2/756042-blink-twice-0-600-0-900-crop.jpg'],
   ['The Assessment', '2024', 'https://letterboxd.com/tinyhomecinema/film/the-assessment-2024/', 'https://a.ltrbxd.com/resized/film-poster/1/1/9/7/8/8/4/1197884-the-assessment-2024-0-600-0-900-crop.jpg'],
   ['Heretic', '2024', 'https://letterboxd.com/tinyhomecinema/film/heretic-2024/', 'https://a.ltrbxd.com/resized/film-poster/1/0/2/5/3/3/1/1025331-heretic-2024-0-600-0-900-crop.jpg'],
-  ['Longlegs', '2024', 'https://letterboxd.com/tinyhomecinema/film/longlegs/', 'https://a.ltrbxd.com/resized/film-poster/1/1/1/0/0/5/9/1110059-longlegs-0-600-0-900-crop.jpg'],
 ]
