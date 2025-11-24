b9a2490df847307f56331e02ae95c6afaf97ee1f
Cineminha <tinyhomecinema@gmail.com>
2025-11-23 21:25:29 -0300

    [auto] Updated data

--- data/media.js
+++ data/media.js
@@ -1,6 +1,5 @@
 var shelves = [
   ['Movies', 421, 'https://letterboxd.com/tinyhomecinema/list/movies-shelf/', 'shelf'],
-  ['TV Shows', 421, 'https://letterboxd.com/tinyhomecinema/list/movies-shelf/', 'shelf'],
   ['Music', 35, 'https://www.librarything.com/catalog.php?view=cineminha&collection=898678&shelf=shelf&sort=stampREV', 'shelf'],
   ['Books', 27, 'https://www.librarything.com/catalog.php?view=cineminha&collection=898806&shelf=shelf', 'shelf']
 ]
