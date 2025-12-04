d872b66158f94d725f9ca93c86388e809c086c15
Cineminha <tinyhomecinema@gmail.com>
2025-12-03 21:00:15 -0300

    [auto] Updated data

--- data/media.js
+++ data/media.js
@@ -11,7 +11,7 @@ var collection = [
   ['Documentaries', 19, 'https://letterboxd.com/tinyhomecinema/list/special-feature-documentaries/', 'collection'],
   ['Blu-ray', 157, 'https://letterboxd.com/tinyhomecinema/list/blu-ray/', 'media'],
   ['DVD', 239, 'https://letterboxd.com/tinyhomecinema/list/dvd/', 'media'],
-  ['VHS', 5, 'https://letterboxd.com/tinyhomecinema/list/decor-vhs-tapes/', 'media'],
+  ['VHS', 239, 'https://letterboxd.com/tinyhomecinema/list/dvd/', 'media'],
   ['Dolby Atmos', 29, 'https://letterboxd.com/tinyhomecinema/list/dolby-atmos/', 'audio'],
   ['DTS X', 13, 'https://letterboxd.com/tinyhomecinema/list/dts-x/', 'audio'],
 ]
