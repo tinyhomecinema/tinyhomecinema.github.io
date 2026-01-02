088aadcc5c68cd028076357aad5bff7060082c34
HaraldoFilho <haraldo.filho@gmail.com>
2026-01-02 07:33:12 -0300

    [auto] Updated data

--- data/media.js
+++ data/media.js
@@ -1,5 +1,6 @@
 var shelves = [
   ['Movies', 421, 'https://letterboxd.com/tinyhomecinema/list/movies-shelf/', 'shelf'],
+  ['TV Shows', 421, 'https://letterboxd.com/tinyhomecinema/list/movies-shelf/', 'shelf'],
   ['Music', 35, 'https://www.librarything.com/catalog.php?view=cineminha&collection=898678&shelf=shelf&sort=stampREV', 'shelf'],
   ['Books', 27, 'https://www.librarything.com/catalog.php?view=cineminha&collection=898806&shelf=shelf', 'shelf']
 ]
@@ -14,4 +15,10 @@ var collection = [
   ['VHS', 5, 'https://letterboxd.com/tinyhomecinema/list/decor-vhs-tapes/', 'media'],
   ['Dolby Atmos', 29, 'https://letterboxd.com/tinyhomecinema/list/dolby-atmos/', 'audio'],
   ['DTS X', 13, 'https://letterboxd.com/tinyhomecinema/list/dts-x/', 'audio'],
+]
+
+var tv_collection = [
+  ['Blu-ray', 13, 'https://letterboxd.com/tinyhomecinema/list/dts-x/', 'media'],
+  ['DVD', 13, 'https://letterboxd.com/tinyhomecinema/list/dts-x/', 'media'],
+  ['Dolby Atmos', 13, 'https://letterboxd.com/tinyhomecinema/list/dts-x/', 'audio'],
 ]
