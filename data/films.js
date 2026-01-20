d777acad521bbd9ce4c635de67808f591370bf74
Cineminha <tinyhomecinema@gmail.com>
2026-01-20 02:25:34 -0300

    [auto] Updated data

---
+++ data/films.js
@@ -0,0 +1,12 @@
+var films = [
+['28 Years Later', '2025', 'https://letterboxd.com/tinyhomecinema/film/28-years-later/', 'https://a.ltrbxd.com/resized/film-poster/9/9/2/7/8/6/992786-28-years-later-0-600-0-900-crop.jpg'],
+  ['Stranger Things 5: The Finale', '2025', 'https://letterboxd.com/tinyhomecinema/film/stranger-things-5-the-finale/', 'https://a.ltrbxd.com/resized/film-poster/1/4/4/0/3/3/4/1440334-stranger-things-5-the-finale-0-600-0-900-crop.jpg'],
+  ['Flow', '2024', 'https://letterboxd.com/tinyhomecinema/film/flow-2024/', 'https://a.ltrbxd.com/resized/film-poster/7/3/9/4/5/1/739451-flow-2024-0-600-0-900-crop.jpg'],
+  ['U Are the Universe', '2024', 'https://letterboxd.com/tinyhomecinema/film/u-are-the-universe/', 'https://a.ltrbxd.com/resized/film-poster/9/1/2/5/1/7/912517-u-are-the-universe-0-600-0-900-crop.jpg'],
+  ['Frankenstein', '2025', 'https://letterboxd.com/tinyhomecinema/film/frankenstein-2025/', 'https://a.ltrbxd.com/resized/film-poster/9/5/8/1/0/0/958100-frankenstein-2025-0-600-0-900-crop.jpg'],
+  ['Superman', '2025', 'https://letterboxd.com/tinyhomecinema/film/superman-2025/', 'https://a.ltrbxd.com/resized/film-poster/9/5/7/0/5/0/957050-superman-2025-0-600-0-900-crop.jpg'],
+  ['The Shadow Strays', '2024', 'https://letterboxd.com/tinyhomecinema/film/the-shadow-strays/', 'https://a.ltrbxd.com/resized/film-poster/1/0/3/8/2/1/8/1038218-the-shadow-strays-0-600-0-900-crop.jpg'],
+  ['How to Make Millions Before Grandma Dies', '2024', 'https://letterboxd.com/tinyhomecinema/film/how-to-make-millions-before-grandma-dies/', 'https://a.ltrbxd.com/resized/film-poster/9/9/5/2/7/4/995274-how-to-make-millions-before-grandma-dies-0-600-0-900-crop.jpg'],
+  ['Gladiator II', '2024', 'https://letterboxd.com/tinyhomecinema/film/gladiator-ii/', 'https://a.ltrbxd.com/resized/film-poster/4/8/6/9/7/8/486978-gladiator-ii-0-600-0-900-crop.jpg'],
+  ['Warfare', '2025', 'https://letterboxd.com/tinyhomecinema/film/warfare/', 'https://a.ltrbxd.com/resized/film-poster/1/1/2/4/7/0/1/1124701-warfare-0-600-0-900-crop.jpg'],
+]
